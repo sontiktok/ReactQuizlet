819d407785e4066bd1b3cfed8ea6c1dbbe9410cc
sontiktok <sonvltiktok@gmail.com>
2024-04-03 00:30:28 +0700

    Design navbar

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.scss";
+import Header from "./components/Header/Header";
+const App = () => {
+  return (
+    <div className="app-container">
+      <Header></Header>
+    </div>
+  );
+};
+
+export default App;
